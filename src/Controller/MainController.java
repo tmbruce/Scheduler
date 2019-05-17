a138641a15f09dcc46354adef24de4052ad2c892
tbruce <tbruce@SWA000004.crwater.local>
2019-05-17 12:05:02 -0700

    Registration success notification

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -8,40 +8,65 @@ package Controller;
 import Model.CalendarTools;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.scene.layout.RowConstraints;
+import javafx.geometry.Insets;
 
 
 
 public class MainController implements Initializable {
 
     
-    
+    @FXML
+    private Button calendarButton;
     @FXML
     private Label monthLabel;
     @FXML
     private GridPane calendarGrid;
     int daysInWeek = 7;
     int weekRows = 6;
+    int monthOffset;
     
     
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        int monthOffset = 0;
+        
+        monthOffset = 0;
         setCalendar(monthOffset);
+        RowConstraints rowConst = new RowConstraints();
+        rowConst.setPrefHeight(calendarGrid.getPrefWidth() / 7);
+        
+        
     }
     
     @FXML
     public void nextMonth(ActionEvent event){
+        monthOffset++;
         clearCalendar();
-        setCalendar(1);
+        setCalendar(monthOffset);
+        calendarButton.requestFocus();
+        
+        
+        
+    }
+    @FXML
+    public void previousMonth(ActionEvent event){
+        monthOffset--;
+        clearCalendar();
+        setCalendar(monthOffset);
+        calendarButton.requestFocus();
     }
     
     public void setCalendar(int monthOffset){
@@ -53,9 +78,20 @@ public class MainController implements Initializable {
         
         //Adds the final days of the previous month to the calendar
         for(int i = 0; i < (monthStartDay - 1); i++){
+            
             Label dayLabel = new Label();
             dayLabel.setText(""+firstCalendarDay);
+            dayLabel.getStyleClass().add("calendarDay");
             VBox dayBox = new VBox();
+            Insets inset = new Insets(3, 3, 3, 3);
+            dayBox.setPadding(inset);
+//            System.out.println(calendarGrid.getPrefWidth());
+//            dayBox.setPrefWidth(calendarGrid.getPrefWidth() / 7);
+//            dayBox.setPrefHeight(calendarGrid.getPrefWidth() / 7);
+            calendarGrid.setVgrow(dayBox, Priority.ALWAYS);
+            calendarGrid.setHgrow(dayBox, Priority.ALWAYS);
+            System.out.println(dayBox.getWidth());
+            dayBox.fillWidthProperty();
             dayBox.getChildren().add(dayLabel);
             calendarGrid.add(dayLabel, i, 1);
             firstCalendarDay++;
@@ -70,6 +106,8 @@ public class MainController implements Initializable {
             VBox dayBox = new VBox();
             dayBox.getChildren().add(dayLabel);
             calendarGrid.add(dayLabel, i, 1);
+            
+            
             firstDayOfMonth++;
             firstDaySecondWeek = firstDayOfMonth;
         }
@@ -91,10 +129,18 @@ public class MainController implements Initializable {
     }
     
     public void clearCalendar(){
-            ObservableList<Node> children = calendarGrid.getChildren();
-            children.clear();
+        ObservableList<Node> nodeList = calendarGrid.getChildren();
+        nodeList.remove(8, nodeList.size());
         }
+        
+        
+    
+    
+    
 }
+            
+        
+
 
     
     
